93f176603800f5e912cc6b20c1228e24e186eb70
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-09 10:50:20 -0300

    refactor urls.py, remove alunos

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from escola.views import alunos
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("alunos/", alunos),
+    path("admin/", admin.site.urls)
 ]
